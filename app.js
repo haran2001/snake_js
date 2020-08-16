e3b101b2cb29e3547e75fec53f4eede1218aac23
haran2001 <hari.ayapps@gmail.com>
2020-08-16 18:44:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,67 @@
+document.addEventListenter("DOMContentLoaded", () => {
+	const squares = document.querySelectorAll('.grid div')
+	const scoreDisplay = document.querySelector('span')	
+	const startBtn = document.querySelector('.start')
+
+	let currentIndex = 0
+	let appleIndex = 0
+	let currentSnake = [2, 1, 0]
+	let direction = 1
+	let score = 0
+	let speed = 0.9
+	let intervalTime = 0
+	let interval = 0
+
+	function startGame(){
+		currentSnake.forEach(index => squares[index].classList.remove('snake'))
+		squares[index].classList.remove('apple')
+		clearInterval(interval)
+		score = 0
+
+		direction = 1
+		scoreDisplay.innerHTML = score
+		intervalTime = 1000
+		currentSnake = [2,1,0]
+		currentIndex = 0
+		currentSnake.forEach(index => squares[index].classList.add('snake'))
+		interval = setInterval(moveOutcomes, intervalTime)
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	function control(e) {
+		squares[currentIndex].classList.remove('snake')
+
+
+		if(e.keyCode === 39) {
+			direction = 1
+		} else if (e.keyCode === 38){
+			direction = -width
+		} else if (e.ketCode === 37){
+			direction = 1
+		} else if (e.keyCode === 40){
+			direction = 10
+		}
+	}
+
+	document.addEventListenter('keyup', control)
+
+})
